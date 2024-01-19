a58071f655abe519456e5de50609b4c2bac2c426
Darren Boyd <boyddarren@protonmail.com>
2024-01-19 19:08:19 +0530

    use TOR proxy for scraping

--- johncena141_releases/pipelines.py
+++ johncena141_releases/pipelines.py
@@ -6,17 +6,8 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-from scrapy.exceptions import DropItem
 
 
 class Johncena141ReleasesPipeline:
-    def __init__(self):
-        self.ids_seen = set()
-
     def process_item(self, item, spider):
-        adapter = ItemAdapter(item)
-        if adapter["id"] in self.ids_seen:
-            raise DropItem(f"Duplicate item found: {item!r}")
-        else:
-            self.ids_seen.add(adapter["id"])
-            return item
+        return item
--- johncena141_releases/settings.py
+++ johncena141_releases/settings.py
@@ -31,7 +31,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
@@ -56,9 +56,9 @@ TELNETCONSOLE_ENABLED = False
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    'johncena141_releases.middlewares.Johncena141ReleasesDownloaderMiddleware': 543,
-# }
+DOWNLOADER_MIDDLEWARES = {
+   'johncena141_releases.middlewares.Johncena141ReleasesDownloaderMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -68,9 +68,9 @@ TELNETCONSOLE_ENABLED = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   'johncena141_releases.pipelines.Johncena141ReleasesPipeline': 300,
-}
+# ITEM_PIPELINES = {
+#    'johncena141_releases.pipelines.Johncena141ReleasesPipeline': 300,
+# }
 
 FEED_EXPORTERS = {
     "rss": "johncena141_releases.exporters.Johncena141ReleasesRSSExporter"
